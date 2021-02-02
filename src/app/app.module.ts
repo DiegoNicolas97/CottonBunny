1b81fafb0db621f027adc05075af38ed5daf9de3
DiegoNicolas97 <diego.rojas9712@gmail.com>
2021-02-02 10:59:10 -0500

    navbar first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './components/pages/landingpage/landingpage.component';
+import { NavbarComponent } from './components/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
